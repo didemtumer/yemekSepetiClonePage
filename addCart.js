838a6f09e233c90653af84cf53d7167567221ee9
didedizman <didemdizman@gmail.com>
2020-09-14 17:40:28 +0300

    Adding style to Tab Content

--- addCart.js
+++ addCart.js
@@ -80,7 +80,7 @@ function addItemToCart(title, price){
         <span class="cart-price cart-column">${price}</span>
         <div class="cart-quantity cart-column">
             <input class="cart-quantity-input" type="number" value="1">
-            <button class="btn btn-danger" type="button">REMOVE</button>
+            <button class="btn btn-danger removeTimes" type="button"><i class="fas fa-times"></i></button>
         </div>`
     cartRow.innerHTML = cartRowContents
     cartItems.append(cartRow)
